f0129fa356e4a4e3aed46c548e851cec6a92e4ee
Eric Avdey <eiri@eiri.ca>
2020-05-17 11:26:27 -0300

    Use pterm to store hmac's key

--- src/seppen_hash.erl
+++ src/seppen_hash.erl
@@ -4,8 +4,14 @@
 
 
 hmac(Value) ->
-    Key = erlang:atom_to_binary(erlang:get_cookie(), unicode),
-    hmac(Key, Value).
+    case persistent_term:get(seppen_key, undefined) of
+        undefined ->
+            Key = erlang:atom_to_binary(erlang:get_cookie(), unicode),
+            persistent_term:put(seppen_key, Key),
+            hmac(Key, Value);
+        Key ->
+            hmac(Key, Value)
+    end.
 
 hmac(Key, Value) ->
     crypto:mac(hmac, sha256, Key, Value).
